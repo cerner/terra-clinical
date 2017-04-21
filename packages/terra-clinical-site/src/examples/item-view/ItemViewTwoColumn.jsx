95e9efa33bb7ae93030e85e3045bc8d0d6b056dd
Dave Kasper <dave.kasper@cerner.com>
2017-04-21 14:52:28 -0500

    Bug fix for sliced elements. (#17)
    *  Bug fix for sliced elements.
    
    * Test name oversight.

--- packages/terra-clinical-site/src/examples/item-view/ItemViewTwoColumn.jsx
+++ packages/terra-clinical-site/src/examples/item-view/ItemViewTwoColumn.jsx
@@ -8,6 +8,9 @@ const display3 = <ItemView.Display text="display 3" />;
 const display4 = <ItemView.Display text="display 4" />;
 const display5 = <ItemView.Display text="display 5" />;
 const display6 = <ItemView.Display text="display 6" />;
-const displays = [display1, display2, display3, display4, display5, display6];
+const display7 = <ItemView.Display text="display 7" />;
+const display8 = <ItemView.Display text="display 8" />;
+const display9 = <ItemView.Display text="display 9" />;
+const displays = [display1, display2, display3, display4, display5, display6, display7, display8, display9];
 
 export default () => <ItemView displays={displays} layout="twoColumns" />;
