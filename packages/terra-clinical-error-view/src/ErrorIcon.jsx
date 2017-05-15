cd06eac41cae183943451e3ffa70cb03dda180c4
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-05-15 16:39:38 -0500

    Change error icon to the svg used in terra-legacy.

---
+++ packages/terra-clinical-error-view/src/ErrorIcon.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React from 'react';
+
+const fillColor = '#e8e9ea';
+
+const ErrorIcon = () => {
+  return (
+    <svg id="Layer_1" data-name="Layer 1" viewBox="0 0 251.7 251.8" style={{ height: '170px', width: '170px' }}>
+    	<path style={{ fill: fillColor}} d="M125.9,0C56.1,0,0,56.2,0,125.9s56,125.9,125.8,125.9s125.9-56.1,125.9-125.9S195.6,0,125.9,0z M36.7,139
+	v-26.2H215V139H36.7z"/>
+    </svg>
+  );
+};
+
+export default ErrorIcon;
+/* eslint-enable */
