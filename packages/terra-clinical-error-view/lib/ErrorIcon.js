4a7560975e8e19c4336c8c168233e86a7991b3da
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-05-16 17:04:14 -0500

    Update svg add height & width attrs and removed style.

--- packages/terra-clinical-error-view/lib/ErrorIcon.js
+++ packages/terra-clinical-error-view/lib/ErrorIcon.js
@@ -16,7 +16,7 @@ var fillColor = '#e8e9ea'; /* eslint-disable */
 var ErrorIcon = function ErrorIcon() {
   return _react2.default.createElement(
     'svg',
-    { id: 'Layer_1', 'data-name': 'Layer 1', viewBox: '0 0 251.7 251.8', style: { height: '170px', width: '170px' } },
+    { id: 'Layer_1', 'data-name': 'Layer 1', viewBox: '0 0 251.7 251.8', height: '170px', width: '170px' },
     _react2.default.createElement('path', { style: { fill: fillColor }, d: 'M125.9,0C56.1,0,0,56.2,0,125.9s56,125.9,125.8,125.9s125.9-56.1,125.9-125.9S195.6,0,125.9,0z M36.7,139 v-26.2H215V139H36.7z' })
   );
 };
