cb483a5226ad750a239419f523753384428156f0
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2017-05-17 07:33:32 -0500

    React 15.5 Compatibility  (#48)
    * Add compatibility for react 15.5
    
    * Update lerna to v2.0.0-rc.4

--- packages/terra-clinical-detail-view/lib/DetailListItem.js
+++ packages/terra-clinical-detail-view/lib/DetailListItem.js
@@ -10,6 +10,10 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 var _classnames = require('classnames');
 
 var _classnames2 = _interopRequireDefault(_classnames);
@@ -26,7 +30,7 @@ var propTypes = {
   /**
    * The item to be displayed.
    **/
-  item: _react.PropTypes.element.isRequired
+  item: _propTypes2.default.element.isRequired
 };
 
 var defaultProps = {
