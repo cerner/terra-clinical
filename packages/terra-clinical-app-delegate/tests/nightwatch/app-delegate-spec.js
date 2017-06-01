a75a50f38119904d112d806cc915df40a728f997
Ryan Manuel <Ryan.Manuel@Cerner.com>
2017-06-01 10:52:37 -0500

    Updates to terra-clinical to fix the Heroku slug size of the project

--- packages/terra-clinical-app-delegate/tests/nightwatch/app-delegate-spec.js
+++ packages/terra-clinical-app-delegate/tests/nightwatch/app-delegate-spec.js
@@ -1,5 +1,6 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 const screenshot = require('terra-toolkit').screenshot;
 
 module.exports = {
