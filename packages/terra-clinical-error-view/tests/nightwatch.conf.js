7d8168530b19deff51c7859478800cf68bcb2148
Ryan Manuel <rfmanuel@gmail.com>
2017-06-01 13:30:07 -0500

    Updates to terra-clinical to fix the Heroku slug size of the project (#80)
    * Updates to terra-clinical to fix the Heroku slug size of the project
    
    * Remove extraneous eslint lines

--- packages/terra-clinical-error-view/tests/nightwatch.conf.js
+++ packages/terra-clinical-error-view/tests/nightwatch.conf.js
@@ -1,7 +1,7 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 const testSettings = require('terra-toolkit').testSettings;
 const resolve = require('path').resolve;
+// eslint-disable-next-line import/no-extraneous-dependencies
 const nightwatchConfiguration = require('terra-toolkit/lib/nightwatch.json');
 
 module.exports = (settings => (
