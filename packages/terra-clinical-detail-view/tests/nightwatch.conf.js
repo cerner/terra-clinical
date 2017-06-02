dda4544e756f340d7605ca5a1dcb5d08f8279f22
Ryan Manuel <rfmanuel@gmail.com>
2017-06-02 13:39:47 -0500

    Individual tests broke when I merged in the nightwatch changes.  This should fix that. (#87)

--- packages/terra-clinical-detail-view/tests/nightwatch.conf.js
+++ packages/terra-clinical-detail-view/tests/nightwatch.conf.js
@@ -4,6 +4,8 @@ const resolve = require('path').resolve;
 // eslint-disable-next-line import/no-extraneous-dependencies
 const nightwatchConfiguration = require('terra-toolkit/lib/nightwatch.json');
 
-module.exports = (settings => (
-  testSettings(resolve('../../webpack.config'), settings)
-))(nightwatchConfiguration);
+module.exports = ((settings) => {
+  const modifiedSettings = settings;
+  modifiedSettings.globals_path = resolve('..', '..', 'node_modules', 'terra-toolkit', 'lib', 'globals');
+  return testSettings(resolve('../../webpack.config'), modifiedSettings);
+})(nightwatchConfiguration);
