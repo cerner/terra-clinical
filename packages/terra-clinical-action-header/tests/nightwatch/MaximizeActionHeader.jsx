cf70b6b5a260df72dfa94e0837990333d01dffd9
Jaime Mackey <jaime.mackey@cerner.com>
2017-06-06 11:16:29 -0500

    Action header - Add AriaLabel Text (#50)
    * Add i18n
    
    * Update translations and jest tests
    
    * Run compile
    
    * Night watch tests
    
    * run compile
    
    * fix night watch tests
    
    * Update to use locale from terra-base
    
    * remove locale prop from examples
    
    * Removed lib files
    
    * Delete aggregated translations folder
    
    * Remove locale prop

--- packages/terra-clinical-action-header/tests/nightwatch/MaximizeActionHeader.jsx
+++ packages/terra-clinical-action-header/tests/nightwatch/MaximizeActionHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Base from 'terra-base';
 import ActionHeader from '../../src/ActionHeader';
 
+const locale = document.getElementsByTagName('html')[0].getAttribute('lang');
+
 const MaximizeHeader = () => (
-  <ActionHeader
-    title="Action Header"
-    onMaximize={() => 1}
-  />
+  <Base locale={locale}>
+    <ActionHeader
+      title="Action Header"
+      onMaximize={() => 1}
+    />
+  </Base>
 );
 
 export default MaximizeHeader;
