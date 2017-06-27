fdc6481dd55a3da3267e121dbdae3dc0a4f94ef3
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2017-06-27 13:24:13 -0500

    Make site examples more realistic. (#108)

--- packages/terra-clinical-site/src/examples/item-view/ItemViewComment.jsx
+++ packages/terra-clinical-site/src/examples/item-view/ItemViewComment.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import IconPerson from 'terra-icon/lib/icon/IconPerson';
 
 import ItemView from 'terra-clinical-item-view';
 
-const comment1 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" />;
+const display1 = <ItemView.Display icon={<IconPerson />} text="Asif Khan" />;
+const display2 = <ItemView.Display text="Care Position: Primary" />;
+const display3 = <ItemView.Display text="Room 100A" />;
+const display4 = <ItemView.Display text="Acuity: 5" />;
+const display5 = <ItemView.Display text="Start Time: 08-05-2016 12:00:00" />;
+const display6 = <ItemView.Display text="End Time: 08-05-2016 16:00:00" />;
+const displays = [display1, display2, display3, display4, display5, display6];
 
-export default () => <ItemView comment={comment1} />;
+const comment = <ItemView.Comment text="Faint red rash appeared at 08-05-2016 13:24:00" />;
+
+export default () => <ItemView displays={displays} comment={comment} />;
