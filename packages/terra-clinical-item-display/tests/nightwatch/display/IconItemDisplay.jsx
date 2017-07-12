2bb6d47b158a74c5f0bc4a4f1b8f5ccf5f3ab047
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-07-12 11:25:10 -0500

    Migrate ItemDisplay to CSS Modules.

--- packages/terra-clinical-item-display/tests/nightwatch/display/IconItemDisplay.jsx
+++ packages/terra-clinical-item-display/tests/nightwatch/display/IconItemDisplay.jsx
@@ -3,4 +3,4 @@ import IconSuccess from 'terra-icon/lib/icon/IconSuccess';
 
 import ItemDisplay from '../../../lib/ItemDisplay';
 
-export default () => <ItemDisplay text="Test Text" icon={<IconSuccess />} />;
+export default () => <ItemDisplay text="Test Text" icon={<IconSuccess />} id="ItemDisplay" />;
