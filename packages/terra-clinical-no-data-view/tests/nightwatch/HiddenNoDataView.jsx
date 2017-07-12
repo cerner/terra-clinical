b35c5de429922597aa294a1641df1c0a3241dcea
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-07-12 08:28:27 -0500

    Add ids for night watch.

--- packages/terra-clinical-no-data-view/tests/nightwatch/HiddenNoDataView.jsx
+++ packages/terra-clinical-no-data-view/tests/nightwatch/HiddenNoDataView.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 
 import NoDataView from '../../lib/NoDataView';
 
-export default () => <NoDataView isGlyphHidden subtextContent={<p>No Data View without the glyph.</p>} />;
+export default () => <NoDataView isGlyphHidden subtextContent={<p>No Data View without the glyph.</p>} id="NoDataView" />;
--- packages/terra-clinical-no-data-view/tests/nightwatch/no-data-view-spec.js
+++ packages/terra-clinical-no-data-view/tests/nightwatch/no-data-view-spec.js
@@ -12,18 +12,24 @@ module.exports = {
 
   'Displays a default no data view': (browser) => {
     browser
-      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/default`);
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/default`)
+      .assert.elementPresent('#NoDataView');
   },
   'Displays a no data view with text for name and description': (browser) => {
     browser
-      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/text`);
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/text`)
+      .assert.elementPresent('#NoDataView')
+      .assert.containsText('#NoDataView p:nth-of-type(1)', 'test heading')
+      .assert.containsText('#NoDataView p:nth-of-type(2)', 'test subtext');
   },
-  'Displays a no data with a button': (browser) => {
+  'Displays a no data view with content': (browser) => {
     browser
-      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/content`);
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/content`)
+      .assert.containsText('#NoDataView p', 'content inserted here');
   },
   'Displays a no data view with glyph hidden': (browser) => {
     browser
-      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/hidden`);
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/no-data-view-tests/hidden`)
+      .assert.elementNotPresent('#NoDataView svg');
   },
 };
