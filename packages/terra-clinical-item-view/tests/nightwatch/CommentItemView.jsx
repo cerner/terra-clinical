e37030bcd1d4ea0e33cd9124db1c5e1a649d13c6
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2017-07-17 09:14:29 -0500

    Migrate Item View to CSS Modules. (#145)

--- packages/terra-clinical-item-view/tests/nightwatch/CommentItemView.jsx
+++ packages/terra-clinical-item-view/tests/nightwatch/CommentItemView.jsx
@@ -3,4 +3,4 @@ import ItemView from '../../lib/ItemView';
 
 const comment1 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment" />;
 
-export default () => <ItemView comment={comment1} />;
+export default () => <ItemView comment={comment1} id="ItemView" />;
