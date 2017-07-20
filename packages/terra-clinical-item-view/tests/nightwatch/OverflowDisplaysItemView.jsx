234852ec81a5cf35acad076748d0280f1d1988bf
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-07-20 11:59:54 -0500

    Update example.

--- packages/terra-clinical-item-view/tests/nightwatch/OverflowDisplaysItemView.jsx
+++ packages/terra-clinical-item-view/tests/nightwatch/OverflowDisplaysItemView.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import ItemView from '../../src/ItemView';
+import IconAlert from 'terra-icon/lib/icon/IconAlert';
+import IconInformation from 'terra-icon/lib/icon/IconInformation';
+import ItemView from '../../lib/ItemView';
 
 const display1 = <ItemView.Display text="display1display1display1display1display1display1display1display1display1display1display1display1display1display1" key="123" />;
 const display2 = <ItemView.Display text="display2display2display2display2display2display2display2display2display2display2display2display2display2display2" key="124" />;
@@ -9,12 +11,12 @@ const display5 = <ItemView.Display text="display 5" key="127" />;
 const display6 = <ItemView.Display text="display 6" key="128" />;
 const displays = [display1, display2, display3, display4, display5, display6];
 
-const accessoryStart = <img alt="Graphic Start" id="StartAccessory" />;
-const accessoryEnd = <img alt="Graphic End" id="EndAccessory" />;
+const accessoryStart = <IconAlert id="StartAccessory" />;
+const accessoryEnd = <IconInformation id="EndAccessory" />;
 const comment = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment" />;
 
 const views = () => (
-  <div style={{ width: '900px' }}>
+  <div style={{ maxWidth: '900px' }}>
     <p>Applied width of 900px to show the default vs truncated styling.</p>
     <br />
     <h2>Full Examples - Default</h2>
