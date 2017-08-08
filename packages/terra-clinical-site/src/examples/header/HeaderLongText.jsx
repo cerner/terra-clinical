f1b65e359f9db75459521f12bd6c590658819019
Kasper, Dave <dave.m.kasper@gmail.com>
2017-08-08 15:42:24 -0500

    Update example.

--- packages/terra-clinical-site/src/examples/header/HeaderLongText.jsx
+++ packages/terra-clinical-site/src/examples/header/HeaderLongText.jsx
@@ -6,7 +6,7 @@ const LongTextWithButtons = () => (
   <div>
     <br />
     <Header
-      title="Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header"
+      title="Lorem ipsum dolor sit amet, sea meis iisque petentium an. Vero copiosae eu eos. Usu ocurreret definiebas eu, vim esse duis habemus at, vix at alii ubique. Nobis meliore principes te cum. Ei quando eirmod oportere eum, ei nonumy eruditi eum. In qui odio euripidis, habeo mundi praesent vim et."
     />
     <br />
   </div>
