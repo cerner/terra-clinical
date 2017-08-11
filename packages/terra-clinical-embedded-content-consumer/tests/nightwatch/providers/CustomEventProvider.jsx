d3a0106fb30ff93cca5ce9e6a282baa312967914
Asthana,Rishi <Rishi.Asthana@Cerner.com>
2017-08-11 10:37:40 -0500

    Fix custom events provider example.

--- packages/terra-clinical-embedded-content-consumer/tests/nightwatch/providers/CustomEventProvider.jsx
+++ packages/terra-clinical-embedded-content-consumer/tests/nightwatch/providers/CustomEventProvider.jsx
@@ -16,7 +16,7 @@ function CustomEventProvider() {
       <meta charSet="utf-8" />
       <style dangerouslySetInnerHTML={{ __html: '\nbody {\npadding-bottom: 5px;\n}\nhtml[hidden] { display: none; }\n' }} />
       <div>
-        <h1>Custom Events</h1>
+        <h1>Custom Event</h1>
         <p>A custom event can be registered with the consumer and intiated by the provider.</p>
         <p>After a one second delay the provider sends the message and consumer handles the message by adding a border.</p>
       </div>
--- packages/terra-clinical-embedded-content-consumer/tests/nightwatch/providers/CustomEventsProvider.jsx
+++ packages/terra-clinical-embedded-content-consumer/tests/nightwatch/providers/CustomEventsProvider.jsx
@@ -16,12 +16,12 @@ function CustomEventProvider() {
 
   return (
     <div>
-      <title>Custom Event Provider</title>
+      <title>Custom Events Provider</title>
       <meta charSet="utf-8" />
       <style dangerouslySetInnerHTML={{ __html: '\nbody {\npadding-bottom: 5px;\n}\nhtml[hidden] { display: none; }\n' }} />
       <div>
         <h1>Custom Events</h1>
-        <p>A custom event can be registered with the consumer and intiated by the provider.</p>
+        <p>Custom events can be registered with the consumer and intiated by the provider.</p>
         <p>After a one second delay the provider sends the message and consumer handles the message by adding a border.</p>
         <p>After another second delay the provider sends the message and consumer handles the message by updating the border.</p>
       </div>
