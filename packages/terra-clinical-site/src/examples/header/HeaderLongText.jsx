4c1b35ffe28fb4b3760553fbbfacd7df9b72b0d5
Dave Kasper <dave.m.kasper@gmail.com>
2017-08-14 15:48:15 -0500

    Header text truncation (#174)
    * Fix truncation
    
    * Add site examples.
    
    * Move examples to header.
    
    * Snapshot and changelog.
    
    * Remove unneeded custom props and div.
    
    * Clean up variable names.
    
    * Update snapshot.
    
    * Update example.
    
    * Add spec test for long title with content.
    
    * Update site example, may not be needed.

---
+++ packages/terra-clinical-site/src/examples/header/HeaderLongText.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-alert */
+import React from 'react';
+import Header from 'terra-clinical-header';
+
+const LongTextWithButtons = () => (
+  <div>
+    <Header
+      title="Lorem ipsum dolor sit amet, sea meis iisque petentium an. Vero copiosae eu eos. Usu ocurreret definiebas eu, vim esse duis habemus at, vix at alii ubique. Nobis meliore principes te cum. Ei quando eirmod oportere eum, ei nonumy eruditi eum. In qui odio euripidis, habeo mundi praesent vim et."
+    />
+  </div>
+);
+
+export default LongTextWithButtons;
--- packages/terra-clinical-site/src/examples/header/Index.jsx
+++ packages/terra-clinical-site/src/examples/header/Index.jsx
@@ -12,6 +12,8 @@ import HeaderSrc from '!raw-loader!terra-clinical-header/src/Header';
 // Example Files
 import TitleHeader from './TitleHeader';
 import ContentHeader from './ContentHeader';
+import HeaderLongText from './HeaderLongText';
+import HeaderLongTextWithContent from './HeaderLongTextWithContent';
 
 const HeaderExamples = () => (
   <div>
@@ -22,6 +24,10 @@ const HeaderExamples = () => (
     <TitleHeader />
     <h1> Header With Content </h1>
     <ContentHeader />
+    <h1> Header With Long Title </h1>
+    <HeaderLongText />
+    <h1> Header With Long Title And Content </h1>
+    <HeaderLongTextWithContent />
   </div>
 );
 
