7c5a6bfaceeecfad755abe8b30ed96b07fca889e
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-08-22 11:39:54 -0500

    Update night watch config.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,15 +1,15 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 const nightwatchConfig = require('terra-toolkit/lib/nightwatch/nightwatch.config.js').default;
 const webpackConfig = require('./packages/terra-clinical-site/webpack.config');
-const getTestDirectories = require('terra-toolkit/lib/nightwatch/setup-helper.js').getTestDirectories;
+const getPackageTestDirectories = require('terra-toolkit/lib/nightwatch/setup-helper.js').getPackageTestDirectories;
 
 let srcFolders;
 const isRepoTest = !process.cwd().includes('/packages/');
 
 if (isRepoTest) {
-  srcFolders = getTestDirectories('lerna.json');
+  srcFolders = getPackageTestDirectories('lerna.json');
 } else {
-  srcFolders = getTestDirectories(process.cwd());
+  srcFolders = 'tests/nightwatch/';
 }
 
 const config = nightwatchConfig(webpackConfig, srcFolders);
