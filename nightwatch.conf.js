a099a725cee5f2f6706802edbf546fdeab193925
Ryan Manuel <rfmanuel@gmail.com>
2017-08-28 13:34:31 -0500

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,7 +1,7 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-const nightwatchConfig = require('terra-toolkit/lib/nightwatch/nightwatch.config.js').default;
+const nightwatchConfig = require('./node_modules/terra-toolkit/lib/nightwatch/nightwatch.config.js').default;
 const webpackConfig = require('./packages/terra-clinical-site/webpack.config');
-const getPackageTestDirectories = require('terra-toolkit/lib/nightwatch/setup-helper.js').getPackageTestDirectories;
+const getPackageTestDirectories = require('./node_modules/terra-toolkit/lib/nightwatch/setup-helper.js').getPackageTestDirectories;
 
 let srcFolders;
 const isRepoTest = !process.cwd().includes('/packages/');
