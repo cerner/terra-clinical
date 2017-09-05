1d47753c236d2b399025d4b74846acd296596c3a
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-09-05 16:18:08 -0500

    Update site example.

--- packages/terra-clinical-site/src/examples/item-collection/ItemCollectionExample.jsx
+++ packages/terra-clinical-site/src/examples/item-collection/ItemCollectionExample.jsx
@@ -3,35 +3,47 @@ import React from 'react';
 import ItemCollection from 'terra-clinical-item-collection';
 import ItemDisplay from 'terra-clinical-item-display';
 import IconAttachment from 'terra-icon/lib/icon/IconAttachment';
-import IconBookmark from 'terra-icon/lib/icon/IconBookmark';
+import IconPerson from 'terra-icon/lib/icon/IconPerson';
+import IconAlert from 'terra-icon/lib/icon/IconAlert';
+import IconInformation from 'terra-icon/lib/icon/IconInformation';
+
+const display1 = <ItemDisplay icon={<IconPerson />} text="Asif Khan" />;
+const display2 = <ItemDisplay text="Care Position: Primary" />;
+const display3 = <ItemDisplay text="Room 100A" />;
+const display4 = <ItemDisplay text="Acuity: 5" />;
+const display5 = <ItemDisplay text="Start Time: 08-05-2016 12:00:00" />;
+const display6 = <ItemDisplay text="End Time: 08-05-2016 16:00:00" />;
+const comment = <ItemDisplay.Comment text="Faint red rash appeared at 08-05-2016 13:24:00" />;
+
+const startAccessory = <IconAlert />;
+const endAccessory = <IconInformation />;
 
 const row1 = {
-  startAccessory: <IconBookmark />,
-  displays: [<ItemDisplay text="Display 1" textStyle="attention" />, <ItemDisplay text="Display 2" />, <ItemDisplay text="Display 3" />],
-  comment: <ItemDisplay.Comment text="Comment" />,
-  endAccessory: <IconAttachment />,
+  startAccessory,
+  displays: [<ItemDisplay icon={<IconPerson />} text="Asif Khan" textStyle="attention" />, display2, display3, display4, display5, display6],
+  comment,
+  endAccessory,
 };
 const row2 = {
-  startAccessory: <IconBookmark />,
-  displays: [<ItemDisplay text="Display 1" />, <ItemDisplay text="Display 2" />, <ItemDisplay text="Display 3" icon={<IconAttachment />} />],
-  comment: <ItemDisplay.Comment text="Comment" />,
+  startAccessory,
+  displays: [display1, display2, display3, <ItemDisplay text="Acuity: 5" icon={<IconAttachment />} />, display5, display6],
+  comment,
   itemStyles: { layout: 'twoColumns' },
 };
 const row3 = {
-  displays: [<ItemDisplay text="Display 1" />, <ItemDisplay text="Display 2" icon={<IconAttachment />} />, <ItemDisplay text="Display 3" />],
-  comment: <ItemDisplay.Comment text="Comment" />,
-  endAccessory: <IconAttachment />,
+  displays: [display1, display2, display3, display4, display5, display6],
+  comment,
+  endAccessory,
 };
 const row4 = {
-  startAccessory: <IconBookmark />,
-  displays: [<ItemDisplay text="Display 1" />, <ItemDisplay text="Display 2" textStyle="strikeThrough" icon={<IconAttachment />} />, <ItemDisplay text="Display 3" />],
-  comment: <ItemDisplay.Comment text="Comment" />,
-  endAccessory: <IconAttachment />,
+  displays: [display1, display2, <ItemDisplay text="Room 100A" textStyle="strikeThrough" />, display4, display5],
+  comment,
+  endAccessory,
 };
 const row5 = {
-  startAccessory: <IconBookmark />,
-  displays: [<ItemDisplay text="Display 1" />, <ItemDisplay text="Display 2" />, <ItemDisplay text="Display 3" />],
-  comment: <ItemDisplay.Comment text="Comment" />,
+  startAccessory,
+  displays: [display1, display2, display3, display4, display5, display6],
+  comment,
   itemStyles: { layout: 'twoColumns' },
 };
 
