22fdbe426e2e79c3625ed4b57470f1797deb6dbf
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-09-07 10:01:13 -0500

    Revert "Simplify changes."
    This reverts commit 3d7ad3b661c0e1aec412812bcecb764c134c86a2.

--- packages/terra-clinical-item-collection/src/_CreateListView.jsx
+++ packages/terra-clinical-item-collection/src/_CreateListView.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import SingleSelectList from 'terra-list/lib/SingleSelectList';
 import ItemView from 'terra-clinical-item-view';
+import determineElements from './_DetermineElements';
 import styles from './ItemCollection.scss';
 
 function createListView(rows, listStyles, selectedIndex, handleSelection) {
-  const numberOfDisplays = rows[0].displays.length;
+  const itemElements = determineElements(rows[0]);
+  const { startAccessoryElement, displayElements, endAccessoryElement } = itemElements;
 
   const listContent = rows.map((row, rowIndex) => {
     const { startAccessory, displays, comment, endAccessory, itemStyles } = row;
 
-    const startAccessoryContent = <div className={styles['list-accessory']}>{startAccessory}</div>;
-    const displayContent = numberOfDisplays ? displays.slice(0, numberOfDisplays) : null;
-    const endAccessoryContent = <div className={styles['list-accessory']}>{endAccessory}</div>;
+    let startAccessoryContent;
+    if (startAccessoryElement) {
+      startAccessoryContent = <div className={styles['list-accessory']}>{startAccessory}</div>;
+    }
+
+    const displayContent = displayElements ? displays.slice(0, displayElements) : null;
+
+    let endAccessoryContent;
+    if (endAccessoryElement) {
+      endAccessoryContent = <div className={styles['list-accessory']}>{endAccessory}</div>;
+    }
 
     const selected = rowIndex === selectedIndex;
     const contentKey = rowIndex;
