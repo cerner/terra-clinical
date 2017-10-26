e9909fdd5de6860401b8a48bbe8fb151c5664e79
Schuster,Kevin <Kevin.Schuster@Cerner.com>
2017-10-26 16:11:30 -0500

    code review updates

--- packages/terra-clinical-header/tests/nightwatch/LargeLeftAndRightContentsWithLargeTitleHeader.jsx
+++ packages/terra-clinical-header/tests/nightwatch/LargeLeftAndRightContentsWithLargeTitleHeader.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-
 import Header from '../../lib/Header';
 
-const startContent = <div id="headerTest--startContent" style={{ backgroundColor: 'black', height: '100px', width: '2000px', margin: '0 10px 0 0' }} />;
-const endContent = <div id="headerTest--endContent" style={{ backgroundColor: 'black', height: '100px', width: '2000px', margin: '0 0 0 10px' }} />;
+const startContent = <div id="headerTest--startContent" style={{ backgroundColor: 'black', height: '100px', width: '200px', margin: '0 10px 0 0' }} />;
+const endContent = <div id="headerTest--endContent" style={{ backgroundColor: 'black', height: '100px', width: '200px', margin: '0 0 0 10px' }} />;
 
 export default () => (
   <Header
