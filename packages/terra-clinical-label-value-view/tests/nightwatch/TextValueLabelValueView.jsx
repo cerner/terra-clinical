94d063b79167ce4a3fb3b8f49d363a2cb6915775
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-11-28 17:27:34 -0600

    Stephen's word was better.

--- packages/terra-clinical-label-value-view/tests/nightwatch/TextValueLabelValueView.jsx
+++ packages/terra-clinical-label-value-view/tests/nightwatch/TextValueLabelValueView.jsx
@@ -6,9 +6,9 @@ const examples = () => (
   <div>
     <h3>Default Example:</h3>
     <LabelValueView id="LabelValueView" label="Label" textValue="Sample Text" />
-    <h3>Applied max-width of 350px to show word wrapping:</h3>
-    <div style={{ maxWidth: '350px' }}>
-      <LabelValueView id="LongLabelValueView" label="A.really.long.label.value.view.sample.text.to.see.it.wrap.correctly." textValue="A.really.long.label.value.view.sample.text.to.see.it.wrap.correctly." />
+    <h3>Applied max-width of 200px to show word wrapping:</h3>
+    <div style={{ maxWidth: '200px' }}>
+      <LabelValueView id="LongLabelValueView" label="supercalifragilisticexpialidocious" textValue="supercalifragilisticexpialidocious" />
     </div>
   </div>
 );
