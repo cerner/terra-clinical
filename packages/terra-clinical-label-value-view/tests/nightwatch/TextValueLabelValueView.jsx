8bf401adaec5aa4ebb72fcb4434182f4da7acbe3
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-11-28 13:35:27 -0600

    Wrap word if a long word is provided.

--- packages/terra-clinical-label-value-view/tests/nightwatch/TextValueLabelValueView.jsx
+++ packages/terra-clinical-label-value-view/tests/nightwatch/TextValueLabelValueView.jsx
@@ -2,4 +2,15 @@ import React from 'react';
 
 import LabelValueView from '../../lib/LabelValueView';
 
-export default () => <LabelValueView id="LabelValueView" label="Label" textValue="Sample Text" />;
+const examples = () => (
+  <div>
+    <h3>Default Example:</h3>
+    <LabelValueView id="LabelValueView" label="Label" textValue="Sample Text" />
+    <h3>Applied max-width of 350px to show word wrapping:</h3>
+    <div style={{maxWidth: '350px' }}>
+      <LabelValueView id="LongLabelValueView" label="A.really.long.label.value.view.sample.text.to.see.it.wrap.correctly." textValue="A.really.long.label.value.view.sample.text.to.see.it.wrap.correctly." />
+    </div>
+  </div>
+);
+
+export default examples;
