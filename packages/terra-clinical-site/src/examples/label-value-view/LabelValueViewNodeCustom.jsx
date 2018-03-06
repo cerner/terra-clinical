77ad1e2b7f42d7a2b9d9ef388599d3547f357b77
Pfeiffer,Neil <neil.pfeiffer@cerner.com>
2018-03-06 15:06:38 -0600

    Label-Value-View updating text size and examples
    Updated value font-size and the site example page to include using
    Terra-Clinical-Item-View

--- packages/terra-clinical-site/src/examples/label-value-view/LabelValueViewNodeCustom.jsx
+++ packages/terra-clinical-site/src/examples/label-value-view/LabelValueViewNodeCustom.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import LabelValueView from 'terra-clinical-label-value-view';
 
-var divStyle = {
+const divStyle = {
   border: '1px solid #d3d4d5',
   borderRadius: '5px',
   padding: '0 20px',
-  width: '280px'
+  width: '280px',
 };
 
 const LabelValueViewNodeCustom = () => (
