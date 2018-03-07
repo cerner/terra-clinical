ad7f0c43f7668a55a955c8efd45b53e634d59ec1
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-03-06 21:38:28 -0600

    Add wdio tests for terra-clinical-application (#301)
    * Wdio tests for terra-clinical-application
    
    * oops

---
+++ packages/terra-clinical-application/tests/wdio/application-spec.js
@@ -0,0 +1,23 @@
+/* global browser, before, Terra */
+
+const viewports = Terra.viewports('tiny', 'small', 'medium', 'large', 'huge', 'enormous');
+
+describe('Clinical Application', () => {
+  describe('Renders the Application with provided AppDelegate', () => {
+    before(() => {
+      browser.url('/#/raw/tests/clinical-application/default-application');
+    });
+
+    Terra.should.matchScreenshot({ viewports });
+    Terra.should.beAccessible();
+  });
+
+  describe('Renders the Application without provided AppDelegate', () => {
+    before(() => {
+      browser.url('/#/raw/tests/clinical-application/no-app-delegate-application');
+    });
+
+    Terra.should.matchScreenshot({ viewports });
+    Terra.should.beAccessible();
+  });
+});
