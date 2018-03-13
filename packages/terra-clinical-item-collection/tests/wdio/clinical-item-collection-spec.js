db18d9895cfa9f13fcbc46d86b6324041940064e
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-03-13 10:16:38 -0500

    update snapshots

--- packages/terra-clinical-item-collection/tests/wdio/clinical-item-collection-spec.js
+++ packages/terra-clinical-item-collection/tests/wdio/clinical-item-collection-spec.js
@@ -1,91 +1,95 @@
 /* eslint no-unused-expressions: 0 */
 /* global before, browser, Terra, expect */
 
-const viewports = Terra.viewports('tiny', 'small', 'medium', 'large', 'huge', 'enormous');
-
-describe('Item Collection', () => {
-  describe('Displays an item collection with all possibe elements', () => {
+Terra.viewports('tiny', 'small', 'medium', 'large', 'huge', 'enormous').forEach((viewport) => {
+  describe('Item Collection', () => {
     before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-all-elements');
+      browser.setViewportSize(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Displays an item collection with all possibe elements', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-all-elements');
+      });
 
-  describe('Displays the correct collection when responding to breakpoint', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-all-elements');
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Displays the correct collection when responding to breakpoint', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-all-elements');
+      });
 
-  describe('Displays a selectable item collection', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Displays a selectable item collection', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
+      });
 
-  describe('Returns child key for click when onSelect is given to a selectable item collection', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
-      browser.click('#item1');
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Returns child key for click when onSelect is given to a selectable item collection', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
+        browser.click('#item1');
+      });
 
-  describe('Returns child key for ENTER when onSelect is given to a selectable item collection', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
-      expect(browser.element('#item2')).to.exist;
-      browser.keys(['\uE004', '\uE004', '\uE004', '\uE007']);
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Returns child key for ENTER when onSelect is given to a selectable item collection', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
+        expect(browser.element('#item2')).to.exist;
+        browser.keys(['\uE004', '\uE004', '\uE004', '\uE007']);
+      });
 
-  describe('Returns child key for SPACE when onSelect is given to a selectable item collection', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
-      expect(browser.element('#item3')).to.exist;
-      browser.keys(['\uE004', '\uE004', '\uE004', '\uE004', '\uE004', '\uE00D']);
+      Terra.should.matchScreenshot();
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Returns child key for SPACE when onSelect is given to a selectable item collection', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-selectable');
+        expect(browser.element('#item3')).to.exist;
+        browser.keys(['\uE004', '\uE004', '\uE004', '\uE004', '\uE004', '\uE00D']);
+      });
 
-  describe('Displays an item collection with table styles', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-table-styles');
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Displays an item collection with table styles', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-table-styles');
+      });
 
-  describe('Displays an item collection with list styles', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-list-styles');
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Displays an item collection with list styles', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-list-styles');
+      });
 
-  describe('Displays an item collection with styles applied to list items', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-item-styles');
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
-  });
+    describe('Displays an item collection with styles applied to list items', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-item-styles');
+      });
 
-  describe('Displays an item collection with items with varying content', () => {
-    before(() => {
-      browser.url('/#/raw/tests/clinical-item-collection/item-collection-varying-items');
+      Terra.should.matchScreenshot(viewport);
     });
 
-    Terra.should.matchScreenshot({ viewports });
+    describe('Displays an item collection with items with varying content', () => {
+      before(() => {
+        browser.url('/#/raw/tests/clinical-item-collection/item-collection-varying-items');
+      });
+
+      Terra.should.matchScreenshot(viewport);
+    });
   });
 });
