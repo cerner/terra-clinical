f5ca940df5963f603c8a0f7ba5216891e9453963
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-03-19 16:37:30 -0500

    common comments

--- packages/terra-clinical-item-view/examples/test-examples/CommentItemView.example.jsx
+++ packages/terra-clinical-item-view/examples/test-examples/CommentItemView.example.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ItemView from '../../lib/ItemView';
 
-const comment1 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment1" />;
-const comment2 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment2" />;
+const comment = id => <ItemView.Comment id={id} text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" />;
 
 const ItemViews = () => (
   <div style={{ width: '250px' }}>
     <h2>Default</h2>
-    <ItemView comment={comment1} id="ItemView1" />
+    <ItemView comment={comment(1)} id="ItemView1" />
     <br />
     <h2>Truncated</h2>
-    <ItemView isTruncated comment={comment2} id="ItemView2" />
+    <ItemView isTruncated comment={comment(2)} id="ItemView2" />
   </div>
 );
 
--- packages/terra-clinical-item-view/examples/test-examples/OverflowDisplaysItemView.example.jsx
+++ packages/terra-clinical-item-view/examples/test-examples/OverflowDisplaysItemView.example.jsx
@@ -11,32 +11,23 @@ const display5 = <ItemView.Display text="display 5" key="127" />;
 const display6 = <ItemView.Display text="display 6" key="128" />;
 const displays = [display1, display2, display3, display4, display5, display6];
 
-const accessoryStart1 = <IconAlert id="StartAccessory1" />;
-const accessoryStart2 = <IconAlert id="StartAccessory2" />;
-const accessoryStart3 = <IconAlert id="StartAccessory3" />;
-const accessoryStart4 = <IconAlert id="StartAccessory4" />;
-const accessoryEnd1 = <IconInformation id="EndAccessory1" />;
-const accessoryEnd2 = <IconInformation id="EndAccessory2" />;
-const accessoryEnd3 = <IconInformation id="EndAccessory3" />;
-const accessoryEnd4 = <IconInformation id="EndAccessory4" />;
-const comment1 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment1" />;
-const comment2 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment2" />;
-const comment3 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment3" />;
-const comment4 = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment4" />;
+const accessoryStart = id => <IconAlert id={id} />;
+const accessoryEnd = id => <IconInformation id={id} />;
+const comment = id => <ItemView.Comment id={id} text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" />;
 
 const views = () => (
   <div style={{ maxWidth: '900px' }}>
     <p>Applied width of 900px to show the default vs truncated styling.</p>
     <br />
     <h2>Full Examples - Default</h2>
-    <ItemView comment={comment1} startAccessory={accessoryStart1} endAccessory={accessoryEnd1} displays={displays} id="ItemView-one-wrap" />
+    <ItemView comment={comment(1)} startAccessory={accessoryStart(5)} endAccessory={accessoryEnd(9)} displays={displays} id="ItemView-one-wrap" />
     <br />
-    <ItemView comment={comment2} startAccessory={accessoryStart2} endAccessory={accessoryEnd2} displays={displays} layout="twoColumns" id="ItemView-two-wrap" />
+    <ItemView comment={comment(2)} startAccessory={accessoryStart(6)} endAccessory={accessoryEnd(10)} displays={displays} layout="twoColumns" id="ItemView-two-wrap" />
     <br />
     <h2>Full Examples - Truncated</h2>
-    <ItemView displays={displays} isTruncated comment={comment3} startAccessory={accessoryStart3} endAccessory={accessoryEnd3} textEmphasis="start" id="ItemView-one-truncate" />
+    <ItemView displays={displays} isTruncated comment={comment(3)} startAccessory={accessoryStart(7)} endAccessory={accessoryEnd(11)} textEmphasis="start" id="ItemView-one-truncate" />
     <br />
-    <ItemView displays={displays} isTruncated comment={comment4} startAccessory={accessoryStart4} endAccessory={accessoryEnd4} layout="twoColumns" textEmphasis="start" id="ItemView-two-truncate" />
+    <ItemView displays={displays} isTruncated comment={comment(4)} startAccessory={accessoryStart(8)} endAccessory={accessoryEnd(12)} layout="twoColumns" textEmphasis="start" id="ItemView-two-truncate" />
   </div>
 );
 
