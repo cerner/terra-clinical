7d42d9453ad3ff8b12430f15fa07183d2af27b80
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-03-20 08:57:25 -0500

    Add wdio tests for terra-clinical-item-view (#304)

--- packages/terra-clinical-item-view/examples/test-examples/CommentItemView.example.jsx
+++ packages/terra-clinical-item-view/examples/test-examples/CommentItemView.example.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ItemView from '../../lib/ItemView';
 
-const comment = <ItemView.Comment text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" id="ItemComment" />;
+const comment = id => <ItemView.Comment id={id} text="comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1comment1" />;
 
 const ItemViews = () => (
   <div style={{ width: '250px' }}>
     <h2>Default</h2>
-    <ItemView comment={comment} id="ItemView1" />
+    <ItemView comment={comment(1)} id="ItemView1" />
     <br />
     <h2>Truncated</h2>
-    <ItemView isTruncated comment={comment} id="ItemView2" />
+    <ItemView isTruncated comment={comment(2)} id="ItemView2" />
   </div>
 );
 
