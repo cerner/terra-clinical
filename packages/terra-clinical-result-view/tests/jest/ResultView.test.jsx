a94895b1ff707134ddb6c0bb481da24411a51e17
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-04-01 22:07:17 -0500

    jest tests

--- packages/terra-clinical-result-view/tests/jest/ResultView.test.jsx
+++ packages/terra-clinical-result-view/tests/jest/ResultView.test.jsx
@@ -1,24 +1,90 @@
 import React from 'react';
+import { IntlProvider } from 'react-intl';
 import ResultView from '../../src/ResultView';
 
-describe('ResultView', () => {
-  const defaultRender = <ResultView />;
+const locale = 'en-US';
+const results = [{ value: '503456789', unit: 'ml', normalcy: 'CRITICAL' }];
+const resultsWithSeperator = [{ value: '120', unit: 'mmHg' }, { value: '80', unit: 'mmHg' }];
 
+describe('ResultView', () => {
   // Snapshot Tests
   it('should render a default component', () => {
-    const wrapper = shallow(defaultRender);
+    const defaultResultView = <IntlProvider locale={locale} ><ResultView /></IntlProvider>;
+    const wrapper = shallow(defaultResultView);
     expect(wrapper).toMatchSnapshot();
   });
 
-  // Prop Tests
-  it('should use the default value when no value is given', () => {
-    const wrapper = shallow(defaultRender);
-    expect(wrapper.find('.result-view').text()).toEqual('default');
+  it('should render a Result View with no time display', () => {
+    const resultView = (
+      <IntlProvider locale={locale} >
+        <ResultView
+          results={results}
+          hasDocument
+          hasComment
+          isPadded
+        />
+      </IntlProvider>);
+    const wrapper = shallow(resultView);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  // Structure Tests
-  it('should have the class result-view', () => {
-    const wrapper = shallow(defaultRender);
-    expect(wrapper.prop('className')).toContain('result-view');
+  it('should render a Result View with truncation by default', () => {
+    const resultView = (
+      <IntlProvider locale={locale} >
+        <ResultView
+          results={results}
+          hasDocument
+          hasComment
+          isPadded
+        />
+      </IntlProvider>);
+    const wrapper = shallow(resultView);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a Result View without truncation', () => {
+    const resultView = (
+      <IntlProvider locale={locale} >
+        <ResultView
+          results={results}
+          hasDocument
+          hasComment
+          isPadded
+          isTruncation={false}
+        />
+      </IntlProvider>);
+    const wrapper = shallow(resultView);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a Result View with separator', () => {
+    const resultView = (
+      <IntlProvider locale={locale} >
+        <ResultView
+          results={resultsWithSeperator}
+          hasDocument
+          hasComment
+          isPadded
+          isTruncation={false}
+        />
+      </IntlProvider>);
+    const wrapper = shallow(resultView);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a Result View with RTL support', () => {
+    const resultView = (
+      <IntlProvider locale={locale} >
+        <ResultView
+          results={results}
+          hasDocument
+          hasComment
+          isPadded
+          isTruncation={false}
+          alignEnd
+        />
+      </IntlProvider>);
+    const wrapper = shallow(resultView);
+    expect(wrapper).toMatchSnapshot();
   });
 });
