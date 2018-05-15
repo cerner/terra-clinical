0bb226ddeb5e0cebb6cf72c136167197e385ace6
Ryan Manuel <rfmanuel@gmail.com>
2018-05-15 16:01:10 -0500

    Create wdio.conf.js

---
+++ wdio.conf.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const wdioConf = require('terra-dev-site/config/wdio/wdio.conf');
+const webpackConfig = require('./webpack.config');
+
+const config = {
+  ...wdioConf.config,
+
+  // Configuration for SeleniumDocker service
+  seleniumDocker: {
+    enabled: true,
+  },
+  
+  host: '0.0.0.0',
+};
+
+exports.config = config;
