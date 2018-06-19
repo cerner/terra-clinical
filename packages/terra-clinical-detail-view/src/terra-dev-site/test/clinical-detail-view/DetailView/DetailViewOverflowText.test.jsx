2870ad66f121fdfeafa1e25b4f3343bd0df86230
Tom Wu <tomleewu@gmail.com>
2018-06-19 13:41:21 -0500

    Added test case for overflow title text

---
+++ packages/terra-clinical-detail-view/src/terra-dev-site/test/clinical-detail-view/DetailView/DetailViewOverflowText.test.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import DetailView from '../../../../DetailView';
+
+const item = (<p>A Detail List Item</p>);
+const item2 = (<p>A Detail List Item to prove float concept when screen readjusts smaller.</p>);
+const accessoryElement = (<p> A sample accessory container </p>);
+
+const DetailViewOverflowText = () => (
+  <DetailView
+    title="OverflowTextOverflowTextOverflowTextOverflowTextOverflowTextOverflowTextOverflowTextOverflowTextOverflowText"
+    secondaryTitles={['secondaryTitle1', 'secondaryTitle2']}
+    subtitles={['Subtitle 1', 'Subtitle 2']}
+    graph={
+      <div
+        style={{
+          border: '1px solid black', height: '20em', width: '25em', marginBottom: '0.714rem',
+        }}
+      >
+      This is where any visualizations would go
+      </div>
+    }
+    accessory={accessoryElement}
+    details={[
+      (
+        <DetailView.DetailList title="Detail List Title" key="detailInfo-1">
+          <DetailView.DetailListItem item={item} />
+          <DetailView.DetailListItem item={item} />
+          <DetailView.DetailListItem item={item2} />
+          <DetailView.DetailListItem item={item} />
+          <DetailView.DetailListItem item={item} />
+        </DetailView.DetailList>
+      ),
+    ]}
+    footer="Footer Text"
+    isDivided={false}
+    id="DetailView"
+    isSmallerTitles={false}
+  />
+);
+
+export default DetailViewOverflowText;
