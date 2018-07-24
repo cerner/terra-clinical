62654717ef97aea8d8588850a96bb7ea0dbf3ac7
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2018-07-24 09:49:57 -0500

    Delete site.config.js

--- packages/terra-dev-site/site.config.js
+++
@@ -1,7 +0,0 @@
-const siteConfig = {
-  appConfig: {
-    locales: ['ar', 'en', 'en-US', 'en-GB', 'es', 'es-US', 'es-ES', 'de', 'fi-FI', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE'],
-  },
-};
-
-module.exports = siteConfig;
