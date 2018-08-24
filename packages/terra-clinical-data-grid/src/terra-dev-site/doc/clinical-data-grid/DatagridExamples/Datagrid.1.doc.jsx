db125c569ad7b449143e4842c4444f6f3b8b2263
Tyler Biethman <tbiethman@gmail.com>
2018-08-24 16:56:18 -0500

    Reverting example structure experiment (whoops)

--- packages/terra-clinical-data-grid/src/terra-dev-site/doc/clinical-data-grid/DatagridExamples/Datagrid.1.doc.jsx
+++ packages/terra-clinical-data-grid/src/terra-dev-site/doc/clinical-data-grid/DatagridExamples/Datagrid.1.doc.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DocTemplate from 'terra-doc-template/lib/DocTemplate';
+import ExampleTemplate from 'terra-doc-template/lib/ExampleTemplate';
 
 import Datagrid from './Datagrid';
 /* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
@@ -7,12 +7,10 @@ import DatagridSrc from '!raw-loader!./Datagrid';
 /* eslint-enable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 
 const DatagridExample = () => (
-  <DocTemplate
-    examples={[{
-      description: 'This is a basic example featuring pinned columns and overflow in both directions. A default column width of 250px is used, but Column 0 is using an explicit 100px width.',
-      example: <Datagrid />,
-      source: DatagridSrc,
-    }]}
+  <ExampleTemplate
+    description="This is a basic example featuring pinned columns and overflow in both directions. A default column width of 250px is used, but Column 0 is using an explicit 100px width."
+    example={<Datagrid />}
+    exampleSrc={DatagridSrc}
   />
 );
 
