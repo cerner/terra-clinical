36ec6758c50973ad328df8d3e18853a65bd49851
Adam Parker <amichaelparker@gmail.com>
2019-01-04 11:43:22 -0600

    changed ordering of dev site doc pages

--- packages/terra-clinical-data-grid/src/terra-dev-site/doc/clinical-data-grid/DatagridExamples.2/DatagridWithoutFill.8.doc.jsx
+++ packages/terra-clinical-data-grid/src/terra-dev-site/doc/clinical-data-grid/DatagridExamples.2/DatagridWithoutFill.8.doc.jsx
@@ -3,7 +3,7 @@ import ExampleTemplate from 'terra-doc-template/lib/ExampleTemplate';
 
 /* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 import DatagridWithoutFill from './DatagridWithoutFill';
-import DatagridWithoutFillSrc from '!raw-loader!../../../../../src/terra-dev-site/doc/clinical-data-grid/DatagridExamples/Datagrid';
+import DatagridWithoutFillSrc from '!raw-loader!../../../../../src/terra-dev-site/doc/clinical-data-grid/DatagridExamples.2/Datagrid';
 /* eslint-enable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 
 const DatagridWithoutFillExample = () => (
