ef5e90f32fb556561d92e97d2101baca9d8b08bf
Adam Parker <amichaelparker@gmail.com>
2019-01-11 11:10:43 -0600

    more markdown script cleaning

--- scripts/generate-markdown/markdown.config.js
+++ scripts/generate-markdown/markdown.config.js
@@ -9,18 +9,18 @@ const header = `| Terra Package      | Version | Status | Dependencies |
 const config = {
   transforms: {
     // see: https://github.com/camacho/markdown-magic-subpackage-list
-    SUBPACKAGELIST(content, _options, configuration) {
-      const packagesDir = path.resolve(
-        path.dirname(configuration.originalPath), './packages',
-      );
+    SUBPACKAGELIST() {
+      const packagesDir = path.join(process.cwd(), 'packages');
 
       return header + fs
         .readdirSync(packagesDir)
-        .map((packageName) => {
-          const packageLink = `[${packageName}](https://github.com/cerner/terra-clinical/tree/master/packages/${packageName})`;
-          const npmBadge = `[![NPM version](https://badgen.net/npm/v/${packageName})](https://www.npmjs.org/package/${packageName})`;
+        .map(filename => path.join(packagesDir, filename))
+        .map(dirPath => JSON.parse(fs.readFileSync(path.resolve(dirPath, 'package.json'))))
+        .map((packages) => {
+          const packageLink = `[${packages.name}](https://github.com/cerner/terra-clinical/tree/master/packages/${packages.name})`;
+          const npmBadge = `[![NPM version](https://badgen.net/npm/v/${packages.name})](https://www.npmjs.org/package/${packages.name})`;
           const buildBadge = '![Stable](https://badgen.net/badge/status/Stable/green)';
-          const dependenciesBadge = `[![${packageName}](https://badgen.net/david/dep/cerner/terra-clinical/packages/${packageName})](https://david-dm.org/cerner/terra-clinical?path=packages/${packageName})`;
+          const dependenciesBadge = `[![${packages.name}](https://badgen.net/david/dep/cerner/terra-clinical/packages/${packages.name})](https://david-dm.org/cerner/terra-clinical?path=packages/${packages.name})`;
 
           return `| ${packageLink} | ${npmBadge} | ${buildBadge} | ${dependenciesBadge} |`;
         })
