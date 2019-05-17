0b3ece17d6fcd5ee0836b0cb8d94bc4e63ab7212
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-05-17 12:47:36 -0500

    stuff

--- jestGlobalSetup.js
+++ jestGlobalSetup.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const aggregateTranslations = require('terra-toolkit/scripts/aggregate-translations/aggregate-translations');
+const aggregateTranslations = require('terra-aggregate-translations');
 
 module.exports = () => {
   aggregateTranslations();
