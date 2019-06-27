fcfbcedbe2852b8fdba3854281a6c19fdd5f4f64
Ryan Manuel <rfmanuel@gmail.com>
2019-06-27 10:43:28 -0500

    Update clinicalItemView.1.doc.jsx

--- packages/terra-clinical-item-view/src/terra-dev-site/doc/clinical-item-view/clinicalItemView.1.doc.jsx
+++ packages/terra-clinical-item-view/src/terra-dev-site/doc/clinical-item-view/clinicalItemView.1.doc.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DocTemplate from 'terra-doc-template';
 import Markdown from 'terra-markdown';
 import ReadMe from '../../../../docs/README.md';
+import ItemViewTwoColumnDocs from '../../../../docs/item-view-two-column.md';
 import { name } from '../../../../package.json';
 
 /* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
