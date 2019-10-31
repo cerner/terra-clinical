d1cb291bf2dcaee82648afa6079260f020c0ae18
neilpfeiffer <neil.pfeiffer@cerner.com>
2019-10-31 03:46:02 -0500

    Update clinical-result-spec.js

--- packages/terra-clinical-result/tests/wdio/clinical-result-spec.js
+++ packages/terra-clinical-result/tests/wdio/clinical-result-spec.js
@@ -7,12 +7,4 @@ Terra.describeViewports('Clinical Result', ['medium'], () => {
 
     Terra.it.validatesElement();
   });
-
-  describe('when bad data is provided', () => {
-    before(() => {
-      browser.url('/#/raw/tests/terra-clinical-result/clinical-result/bad-data-clinical-result');
-    });
-
-    Terra.it.validatesElement();
-  });
 });
