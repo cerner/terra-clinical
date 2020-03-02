a946b3b3eac5a238fba7c3584c69237aa5ecaa50
neilpfeiffer <neil.pfeiffer@cerner.com>
2020-03-02 06:00:16 -0600

    Adding colon

--- packages/terra-clinical-result/src/proptypes/statusPropTypes.js
+++ packages/terra-clinical-result/src/proptypes/statusPropTypes.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 /**
  * Enum for possible Result Statuses.
- * values mapped from http //hl7.org/fhir/2018Sep/valueset-observation-status.html
+ * values mapped from http://hl7.org/fhir/2018Sep/valueset-observation-status.html
  */
 const statusPropType = PropTypes.oneOf([
   'registered',
