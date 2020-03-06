178f1277f7542e0d4b8c267759fb07c2a6fa8ebe
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2020-03-06 16:26:11 -0600

    Update props to be cleaner

--- packages/terra-clinical-result/src/proptypes/valuePropTypes.js
+++ packages/terra-clinical-result/src/proptypes/valuePropTypes.js
@@ -5,7 +5,7 @@ const valueQuantityPropType = PropTypes.shape({
    *  Value for a single Observation Result.
    *  can be either single string or array of string values for multi-alpha responses
    */
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
   /**
    *  Unit of Measure representation for an Observation Result.
    */
