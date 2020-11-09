aef61aae26215b622b3cf5481c59485ca4b5753d
Matt Henkes <mjhenkes@gmail.com>
2020-11-09 12:42:27 -0600

    Apply suggestions from code review

--- packages/terra-clinical-detail-view/tests/wdio/detail-list-item-spec.js
+++ packages/terra-clinical-detail-view/tests/wdio/detail-list-item-spec.js
@@ -1,6 +1,6 @@
 Terra.describeViewports('Detail List Item', ['tiny', 'small', 'medium', 'large', 'huge'], () => {
   it('Displays a Detail List Item as expected', () => {
-    browser.url('/#/raw/tests/terra-clinical-detail-view/clinical-detail-view/detail-list-item/default-list-item');
+    browser.url('/raw/tests/terra-clinical-detail-view/clinical-detail-view/detail-list-item/default-list-item');
 
     Terra.validates.element('default');
   });
--- packages/terra-clinical-detail-view/tests/wdio/detail-list-spec.js
+++ packages/terra-clinical-detail-view/tests/wdio/detail-list-spec.js
@@ -2,13 +2,13 @@ const selector = '#root';
 
 Terra.describeViewports('Detail List', ['tiny', 'small', 'medium', 'large', 'huge'], () => {
   it('Displays a default Detail List with a title and list', () => {
-    browser.url('/#/raw/tests/terra-clinical-detail-view/clinical-detail-view/detail-list/normal-item-list');
+    browser.url('/raw/tests/terra-clinical-detail-view/clinical-detail-view/detail-list/normal-item-list');
 
     Terra.validates.element('title and list', { selector });
   });
 
   it('Displays a default Detail List with a title and large list', () => {
-    browser.url('/#/raw/tests/terra-clinical-detail-view/clinical-detail-view/detail-list/large-item-list');
+    browser.url('/raw/tests/terra-clinical-detail-view/clinical-detail-view/detail-list/large-item-list');
 
     Terra.validates.element('title and large list', { selector });
   });
