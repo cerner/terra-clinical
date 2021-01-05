3dbade1958eeee97ef85f8a03708a62015856bb0
Matt Henkes <mjhenkes@gmail.com>
2021-01-05 13:56:31 -0600

    Use new webpack-config-terra package (#711)
    * Use new webpack-config-terra package
    
    * validate theme aggregation in webpack-config-terra
    
    * use released version
    
    * full fill postcss peer

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,48 @@
-const terraDevSiteWebpackConfig = require('terra-dev-site/config/webpack/webpack.config');
+/* eslint-disable import/no-extraneous-dependencies */
+const { merge } = require('webpack-merge');
+const path = require('path');
+const {
+  TerraDevSite,
+  TerraDevSiteEntrypoints,
+  DirectorySwitcherPlugin,
+  LocalPackageAliasPlugin,
+} = require('terra-dev-site');
 
-module.exports = terraDevSiteWebpackConfig;
+const WebpackConfigTerra = require('@cerner/webpack-config-terra');
+/**
+* Generates the file representing app name configuration.
+*/
+const devSiteConfig = (env = {}, argv = { p: false }) => {
+  const production = argv.p;
+  const processPath = process.cwd();
+
+  return {
+    entry: TerraDevSiteEntrypoints,
+    plugins: [
+      new TerraDevSite({ env }),
+    ],
+    resolve: {
+      plugins: [
+        new DirectorySwitcherPlugin({
+          shouldSwitch: !production,
+          rootDirectories: [
+            processPath,
+            path.resolve(processPath, 'packages', '*'),
+          ],
+        }),
+        new LocalPackageAliasPlugin({
+          rootDirectories: [
+            processPath,
+            path.resolve(processPath, 'packages', '*'),
+          ],
+        }),
+      ],
+    },
+  };
+};
+
+const webpackConfig = (env, argv) => (
+  merge(WebpackConfigTerra(env, argv), devSiteConfig(env, argv))
+);
+
+module.exports = webpackConfig;
