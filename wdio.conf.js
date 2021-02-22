93a4eb63fd10c6e952cb119a8b9820b47f319df6
Stephen Esser <Stephen.Esser@gmail.com>
2021-02-22 16:02:44 -0600

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,12 +8,4 @@ if (process.env.npm_package_name !== 'terra-clinical') {
   wdioConfig.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
 }
 
-wdioConfig.specs = [
-  './packages/terra-clinical-data-grid/tests/wdio/data-grid-spec.js',
-  './packages/terra-clinical-detail-view/tests/wdio/*.js',
-  './packages/terra-clinical-header/tests/wdio/*.js',
-  './packages/terra-clinical-item-collection/tests/wdio/*.js',
-  './packages/terra-clinical-item-display/tests/wdio/*.js',
-];
-
 exports.config = wdioConfig;
