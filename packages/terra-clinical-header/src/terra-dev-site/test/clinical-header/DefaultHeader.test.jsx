39c042da0203ae000d21906312253252f270dbbb
saket2403 <saket.bajaj1998@gmail.com>
2022-02-18 11:02:30 +0530

    Fix wdio

--- packages/terra-clinical-header/src/terra-dev-site/test/clinical-header/DefaultHeader.test.jsx
+++ packages/terra-clinical-header/src/terra-dev-site/test/clinical-header/DefaultHeader.test.jsx
@@ -2,8 +2,4 @@ import React from 'react';
 
 import Header from '../../../Header';
 
-export default () => (
-  <div aria-labelledby="Header">
-    <Header text="Test-Header" id="Header" level={1} />
-  </div>
-);
+export default () => <Header id="Header" />;
