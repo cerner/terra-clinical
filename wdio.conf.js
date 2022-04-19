8fda2a6cfd0cde34703d13a03ce859c9b49343a3
Ben Cai <benbcai@gmail.com>
2022-04-18 21:51:33 -0500

    Revert wdio.config.fs.

--- wdio.conf.js
+++ wdio.conf.js
@@ -2,10 +2,8 @@ const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { config } = require('@cerner/terra-functional-testing/lib/config/wdio.conf');
 
-// if (process.env.npm_package_name !== 'terra-clinical') {
-//   config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
-// }
-
-config.specs = [path.join(__dirname, 'packages', 'terra-clinical-data-grid', 'tests', 'wdio', '**', '*-spec.js')];
+if (process.env.npm_package_name !== 'terra-clinical') {
+  config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
+}
 
 exports.config = config;
