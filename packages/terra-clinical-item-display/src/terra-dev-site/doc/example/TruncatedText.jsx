08908d6c569a6a09186c041b7eabfc9dab10bd86
SC069282 <sydneyycombs@gmail.com>
2023-05-17 10:06:25 -0500

    Use terra-show-hide

--- packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TruncatedText.jsx
+++ packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TruncatedText.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 import ItemDisplay from 'terra-clinical-item-display';
-import Button from 'terra-button';
+import ShowHide from 'terra-show-hide';
 
 const sampleText = 'Mr. James is currently receiving outpatient treatment. He has been diagnosed with an Axis I diagnosis of Psychosis NOS, ruled out Schizoaffective Disorder and Post Traumatic Stress Disorder, and is being treated with Haldol 5mg and Cogentin 1mg.';
 
 const TruncatedTextExample = () => {
-  const [truncated, setTruncated] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
-    setTruncated(!truncated);
+  const handleChange = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
     <>
-      <ItemDisplay text={sampleText} isTruncated={truncated} />
-      <br />
-      <Button text="Show/Hide" onClick={handleClick} />
+      <ShowHide preview={<ItemDisplay text={sampleText} isTruncated />} isOpen={isOpen} onChange={handleChange}>
+        {sampleText}
+      </ShowHide>
     </>
   );
 };
