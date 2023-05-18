2a49c03ca8e7066a8fd810201f2e86b5352f70f6
Mason, Steven <steven.mason@oracle.com>
2023-05-18 16:37:53 -0500

    Update docs

--- packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TextStyleMeaning.jsx
+++ packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TextStyleMeaning.jsx
@@ -4,13 +4,13 @@ import ItemDisplay from 'terra-clinical-item-display';
 const component = () => (
   <React.Fragment>
     <ItemDisplay text="Attention" textStyle="attention" textStyleMeaning="Important" />
-    <p>Screen reader reads: &ldquo;Important: Attention; Important End.&rdquo;</p>
+    <p>Screen reader reads: &ldquo;Important&rdquo;, &ldquo;Attention&rdquo;, &ldquo;Important End&rdquo;</p>
     <br />
     <ItemDisplay text="Strike-through (Default)" textStyle="strikeThrough" />
-    <p>Screen reader reads: &ldquo;Deletion: Strike-through (Default); Deletion End.&rdquo;</p>
+    <p>Screen reader reads: &ldquo;Deletion&rdquo;, &ldquo;Strike-through (Default)&rdquo;, &ldquo;Deletion End&rdquo;</p>
     <br />
     <ItemDisplay text="Strike-through" textStyle="strikeThrough" textStyleMeaning="Update" />
-    <p>Screen reader reads: &ldquo;Update: Strike-through; Update End.&rdquo;</p>
+    <p>Screen reader reads: &ldquo;Update&rdquo;, &ldquo;Strike-through&rdquo;, &ldquo;Update End&rdquo;</p>
   </React.Fragment>
 );
 
