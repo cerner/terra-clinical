44febfedf6e401c824c621e5abcbfca3b6021224
MadalinaStelea <93373012+MadalinaStelea@users.noreply.github.com>
2023-05-23 19:45:59 +0300

    Fix lint issues

--- packages/terra-clinical-label-value-view/src/terra-dev-site/test/clinical-label-value-view/TextValueLabelValueView.test.jsx
+++ packages/terra-clinical-label-value-view/src/terra-dev-site/test/clinical-label-value-view/TextValueLabelValueView.test.jsx
@@ -8,7 +8,7 @@ const cx = classNames.bind(styles);
 
 const examples = () => (
   <div>
-    <h3>LabelValueView returned as a term and a definition (childOfDescriptionList="true"):</h3>
+    <h3>LabelValueView returned as a term and a definition (childOfDescriptionList='true'):</h3>
     <dl>
       <LabelValueView label="Collected By" textValue="Noah Brown" childOfDescriptionList="true" />
     </dl>
@@ -24,16 +24,13 @@ const examples = () => (
     <dl>
       <LabelValueView label="Heart Rate" textValue="80 BPM" childOfDescriptionList="true" />
     </dl>
-
-    <h3>LabelValueView returned as a description list (childOfDescriptionList="false"):</h3>
+    <h3>LabelValueView returned as a description list (childOfDescriptionList='false'):</h3>
     <LabelValueView label="Collected By" textValue="Noah Brown" childOfDescriptionList="false" />
     <LabelValueView label="Date" textValue="12/12/12 00:00AM" childOfDescriptionList="false" />
-    
     <h3>LabelValueView returned as a description list (childOfDescriptionList is undefined - not sent):</h3>
     <LabelValueView label="Blood Pressure" textValue="110/60 mmHg" />
     <LabelValueView label="Temperature" textValue="97 F" />
     <LabelValueView label="Heart Rate" textValue="80 BPM" />
-    
     <h3>Applied max-width of 200px to show word wrapping:</h3>
     <div className={cx('labelvalueview-wrapper')}>
       <LabelValueView id="LongLabelValueView" label="supercalifragilisticexpialidocious" textValue="supercalifragilisticexpialidocious" />
