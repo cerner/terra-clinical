39f7ad2ee5b0adcfe6f98c486b7b277b82650799
ry061521 <reagan.young@oracle.com>
2023-07-13 09:52:51 -0500

    Remove WDIO test for semantic table

--- packages/terra-clinical-result/tests/wdio/flowsheet-result-cell-spec.js
+++ packages/terra-clinical-result/tests/wdio/flowsheet-result-cell-spec.js
@@ -65,12 +65,6 @@ Terra.describeViewports('FlowsheetResultCell', ['medium'], () => {
     Terra.validates.element('entered in error');
   });
 
-  it('displays a FlowsheetResultCell in a semantic table', () => {
-    browser.url('/#/raw/tests/terra-clinical-result/clinical-result/flowsheet-result-cell/semantic-table-with-flowsheet-result-cell');
-
-    Terra.validates.element('with semantic table');
-  });
-
   it('displays a FlowsheetResultCell in a semantic table with multiple padding styles', () => {
     browser.url('/#/raw/tests/terra-clinical-result/clinical-result/flowsheet-result-cell/semantic-paddingstyle-with-flowsheet-result-cell');
 
