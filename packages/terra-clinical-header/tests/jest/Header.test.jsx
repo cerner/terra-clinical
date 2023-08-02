ff023d48ccd28325f5280898f6f631faa9b096ab
SM051274 <sm051274@cerner.net>
2023-08-02 15:37:56 +0530

    added afterEach for clearing mocks

--- packages/terra-clinical-header/tests/jest/Header.test.jsx
+++ packages/terra-clinical-header/tests/jest/Header.test.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Header from '../../src/Header';
 
-const consoleSpy = jest.spyOn(global.console, 'warn');
+afterEach(() => {
+  // restore the spy created with spyOn
+  jest.restoreAllMocks();
+});
 
 it('should render a default component', () => {
   const header = render(<Header />);
@@ -36,6 +39,7 @@ it('should render a header with all content', () => {
 });
 
 it('should render a subheader with title and heading level', () => {
+  const consoleSpy = jest.spyOn(global.console, 'warn');
   const subheader = render(<Header level={1} title="title" isSubheader />);
   const titleWarningMessage = 'The `title` prop has been renamed to `text`. Please update all references of `title` prop to `text`.';
   expect(consoleSpy).toHaveBeenCalledWith(titleWarningMessage);
@@ -56,6 +60,7 @@ it('should render a subheader with all content', () => {
 });
 
 it('should render a header with default heading level when level not set', () => {
+  const consoleSpy = jest.spyOn(global.console, 'warn');
   const header = render(<Header text="This title should render with a default level" />);
   const levelWarningMessage = 'Default heading level may not appropriate has it would fail to convey context of heading in a site / application where it is used. Heading level should be set explicitly depending on the position of header in site / application to allow screen readers to identify headers consistently.';
   expect(consoleSpy).toHaveBeenCalledWith(levelWarningMessage);
