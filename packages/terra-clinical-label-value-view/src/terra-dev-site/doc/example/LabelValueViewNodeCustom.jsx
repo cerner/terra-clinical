57a86f5295fb62b3b28c082cc34f123ffd7bb9dc
JonathanSatinger <127299073+JonathanSatinger@users.noreply.github.com>
2023-09-07 11:30:41 +0300

    Update LabelValueViewNodeCustom.jsx

--- packages/terra-clinical-label-value-view/src/terra-dev-site/doc/example/LabelValueViewNodeCustom.jsx
+++ packages/terra-clinical-label-value-view/src/terra-dev-site/doc/example/LabelValueViewNodeCustom.jsx
@@ -10,7 +10,7 @@ const LabelValueViewNodeCustom = () => (
     <LabelValueView label="Daily Feeds">
       <div className={cx('label-value-view-content')}>
         <h3>Critical Alert: Covid-19 Outbreak</h3>
-        <p>Due to recent Covid-19 outbreakm out healthcare facility has implemented strict protocals to ensure the safety and well-being of our patients and staff.</p>
+        <p>Due to recent Covid-19 outbreak our healthcare facility has implemented strict protocols to ensure the safety and well-being of our patients and staff.</p>
       </div>
     </LabelValueView>
   </div>
