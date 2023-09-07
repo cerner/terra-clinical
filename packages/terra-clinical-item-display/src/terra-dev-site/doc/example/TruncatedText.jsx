8774eceee376e0a91f19e62db3c6212aa0fcb47f
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-09-07 18:20:43 -0400

    ShowHide example updated for accessibility

--- packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TruncatedText.jsx
+++ packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TruncatedText.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ItemDisplay from 'terra-clinical-item-display';
-import ShowHide from 'terra-show-hide';
+import ShowHide, { ShowHideFocuser } from 'terra-show-hide';
 
 const previewText = 'Mr. James is currently receiving outpatient treatment. He has been diagnosed with an Axis I diagnosis of Psychosis NOS, ruled out Schizoaffective Disorder and Post Traumatic Stress Disorder, and is being treated with Haldol 5mg and Cogentin 1mg.';
-const sampleText = 'Mr. James is currently receiving outpatient treatment. He has been diagnosed with an Axis I diagnosis of Psychosis NOS, ruled out Schizoaffective Disorder and Post Traumatic Stress Disorder, and is being treated with Haldol 5mg and Cogentin 1mg. His treatment may have potential side effects such as dizziness and headaches. His symptoms have been manageable, if any side effects or concerns come up, consult with the doctor immediately.';
+const sampleText = 'His treatment may have potential side effects such as dizziness and headaches. His symptoms have been manageable, if any side effects or concerns come up, consult with the doctor immediately.';
 
 const TruncatedTextExample = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const focusRef = useRef(null);
 
   const handleChange = () => {
     setIsOpen(!isOpen);
@@ -17,8 +18,9 @@ const TruncatedTextExample = () => {
       preview={<ItemDisplay text={previewText} isTruncated />}
       isOpen={isOpen}
       onChange={handleChange}
+      focusRef={focusRef}
     >
-      {sampleText}
+      <ShowHideFocuser ref={focusRef} prefix={previewText} focusableText={sampleText} />
     </ShowHide>
   );
 };
