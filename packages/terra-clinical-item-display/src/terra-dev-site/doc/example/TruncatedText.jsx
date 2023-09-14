fead8e19180f7dbb89b1eb3016f451f03b2bc454
Andrew Tran <Andrew.Tran2@cerner.com>
2023-09-14 15:16:19 +0000

    Nit

--- packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TruncatedText.jsx
+++ packages/terra-clinical-item-display/src/terra-dev-site/doc/example/TruncatedText.jsx
@@ -9,7 +9,7 @@ import { DisclosureManagerContext, DisclosureManagerHeaderAdapter } from 'terra-
 
 const fullText = 'Mr. James is currently receiving outpatient treatment. He has been diagnosed with an Axis I diagnosis of Psychosis NOS, ruled out Schizoaffective Disorder and Post Traumatic Stress Disorder, and is being treated with Haldol 5mg and Cogentin 1mg. His treatment may have potential side effects such as dizziness and headaches. His symptoms have been manageable, if any side effects or concerns come up, consult with the doctor immediately.';
 
-const TruncatedTextExample = () => {
+const TruncatedText = () => {
   const [popupOpen, setPopupOpen] = useState(false);
   const popupRef = useRef(null);
 
@@ -107,4 +107,4 @@ const TruncatedTextExample = () => {
   );
 };
 
-export default TruncatedTextExample;
+export default TruncatedText;
