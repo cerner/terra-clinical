cf417ee24335fd1281d4babcd523b431e82945ea
ry061521 <reagan.young@oracle.com>
2023-09-22 09:24:03 -0500

    Add 'render' to test titles

--- packages/terra-clinical-item-view/tests/wdio/item-view-spec.js
+++ packages/terra-clinical-item-view/tests/wdio/item-view-spec.js
@@ -1,5 +1,5 @@
 Terra.describeViewports('Clinical Item View', ['tiny', 'small', 'medium', 'large', 'huge', 'enormous'], () => {
-  it('with displays', () => {
+  it('renders with displays', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/displays-item-view');
 
     Terra.validates.element('with one column', { selector: '#test-displays' });
@@ -7,40 +7,40 @@ Terra.describeViewports('Clinical Item View', ['tiny', 'small', 'medium', 'large
     Terra.validates.element('with two column and start displays', { selector: '#test-displays-two-start' });
   });
 
-  it('with displays by row', () => {
+  it('renders with displays by row', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/displays-item-view-by-row');
 
     Terra.validates.element('with two columns by row', { selector: '#test-displays-two-row' });
     Terra.validates.element('with two columns and start displays by row', { selector: '#test-displays-two-row-start' });
   });
 
-  it('with accessories', () => {
+  it('renders with accessories', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/accessory-item-view');
 
     Terra.validates.element('with accessories');
   });
 
-  it('with default comment set', () => {
+  it('renders with default comment set', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/comment-item-view');
 
     Terra.validates.element('comment', { selector: '#ItemView1' });
     Terra.validates.element('truncated comment', { selector: '#ItemView2' });
   });
 
-  it('with word wrap', () => {
+  it('renders with word wrap', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/overflow-displays-item-view');
 
     Terra.validates.element('with word wrap in one column', { selector: '#ItemView-one-wrap' });
     Terra.validates.element('with word wrap in two columns', { selector: '#ItemView-two-wrap' });
   });
 
-  it('with word wrap by row', () => {
+  it('renders with word wrap by row', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/overflow-displays-item-view-by-row');
 
     Terra.validates.element('with word wrap in two columns by row', { selector: '#ItemView-by-row-two-wrap' });
   });
 
-  it('with override default styling', () => {
+  it('renders with override default styling', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/styling-override-item-view');
 
     Terra.validates.element('with override styling');
@@ -48,7 +48,7 @@ Terra.describeViewports('Clinical Item View', ['tiny', 'small', 'medium', 'large
 });
 
 Terra.describeViewports('Clinical Item View', ['enormous'], () => {
-  it('with truncation', () => {
+  it('renders with truncation', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/overflow-displays-item-view');
 
     Terra.validates.element('with truncation in one column', { selector: '#ItemView-one-truncate' });
@@ -57,7 +57,7 @@ Terra.describeViewports('Clinical Item View', ['enormous'], () => {
 });
 
 Terra.describeViewports('Clinical Item View', ['enormous'], () => {
-  it('with by row truncation', () => {
+  it('renders with by row truncation', () => {
     browser.url('/raw/tests/terra-clinical-item-view/clinical-item-view/overflow-displays-item-view-by-row');
 
     Terra.validates.element('with truncation in two columns by row', { selector: '#ItemView-by-row-two-truncate' });
