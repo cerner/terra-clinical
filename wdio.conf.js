9d412d836fcb096ea089449d2e59c51a8dd0ca99
Saad <38024451+sdadn@users.noreply.github.com>
2024-05-13 15:07:21 -0500

    updated wdio config

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,9 +1,6 @@
-const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { config } = require('@cerner/terra-functional-testing/lib/config/wdio.conf');
+const { config } = require('@cerner/terra-functional-testing');
 
-if (process.env.npm_package_name !== 'terra-clinical') {
-  config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
-}
+config.specs = ['./packages/**/tests/wdio/**/*-spec.js'];
 
 exports.config = config;
